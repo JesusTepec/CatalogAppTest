352bbdd53359382edf7f8f01eba15e392b5a8d02
Jesus Tepec <ttpecjn@gmail.com>
2021-05-29 05:38:19 -0500

    Feat: add file AppConstants.kt

---
+++ app/src/main/java/com/catalogapptest/AppConstants.kt
@@ -0,0 +1,14 @@
+package com.catalogapptest
+
+
+const val TOKEN = ""
+const val SKILL_ID = 0
+const val BABY_ID = 0
+
+const val KEY_TOKEN = "token_rest"
+const val KEY_SKILL_ID = "skill_id"
+const val KEY_BABY_ID = "baby_id"
+
+const val GET_ACTIVITIES = "/api/v3/catalogue/activities"
+const val GET_ARTICLES = "/api/v3/catalogue/articles"
+const val GET_ARTICLES_DETAIL = "/api/v3/articles"
